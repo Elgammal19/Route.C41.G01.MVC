ac70cb7a257ee0f08dc9d612f4c07f79f087777a
Mohamed Elgammal <mohamadalgamal1919@gmail.com>
2024-04-27 10:29:23 +0300

    Send Email

--- Route.C41.G02.MVC03.PL/Helpers/EmailSettings.cs
+++ Route.C41.G02.MVC03.PL/Helpers/EmailSettings.cs
@@ -1,4 +1,6 @@
 ﻿using Route.C41.G02.DAL.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace Route.C41.G02.MVC03.PL.Helpers
 {
@@ -6,7 +8,13 @@ namespace Route.C41.G02.MVC03.PL.Helpers
 	{
 		public static void SendEmail(Email email)
 		{
+			// Mail Server --> Gmail 
+			var client = new SmtpClient("smtp.gmail.com", 587);
+			
+			client.EnableSsl = true;
+			client.Credentials = new NetworkCredential("mohammedelgammal003@gmail.com", "yaxy apem gnkr vknl");
 
+			client.Send("mohammedelgammal003@gmail.com", email.Recipients, email.Subject, email.Body);
 		}
 
 	}
--- Route.C41.G02.MVC03.PL/ViewModels/Account/ResetPasswordViewModel.cs
+++ Route.C41.G02.MVC03.PL/ViewModels/Account/ResetPasswordViewModel.cs
@@ -1,18 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿//using System.ComponentModel.DataAnnotations;
 
-namespace Route.C41.G02.MVC03.PL.ViewModels.Account
-{
-    public class ResetPasswordViewModel
-    {
-        [Required(ErrorMessage = "Password Is Required")]
-        [MinLength(6, ErrorMessage = "Minimum Password Length Is 6")]
-        [DataType(DataType.Password)]
-        public string NewPassword { get; set; }
+//namespace Route.C41.G02.MVC03.PL.ViewModels.Account
+//{
+//    public class ResetPasswordViewModel
+//    {
+//        [Required(ErrorMessage = "Password Is Required")]
+//        [MinLength(6, ErrorMessage = "Minimum Password Length Is 6")]
+//        [DataType(DataType.Password)]
+//        public string NewPassword { get; set; }
 
-        [Required(ErrorMessage = "ConfirmPassword Is Required")]
-        [MinLength(6, ErrorMessage = "Minimum ConfirmPassword Length Is 6")]
-        [DataType(DataType.Password)]
-        [Compare(nameof(NewPassword), ErrorMessage = "ConfirmPassword Doesn't Match Password")]
-        public string ConfirmPassword { get; set; }
-    }
-}
+//        [Required(ErrorMessage = "ConfirmPassword Is Required")]
+//        [MinLength(6, ErrorMessage = "Minimum ConfirmPassword Length Is 6")]
+//        [DataType(DataType.Password)]
+//        [Compare(nameof(NewPassword), ErrorMessage = "ConfirmPassword Doesn't Match Password")]
+//        public string ConfirmPassword { get; set; }
+//    }
+//}
